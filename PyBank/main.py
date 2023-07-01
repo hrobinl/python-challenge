de8b9bc4e3c6725e91d3045d3fb30a3f6c363dad
hrobinl <132854695+hrobinl@users.noreply.github.com>
2023-07-01 11:26:20 -0400

    attempt

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,11 +5,28 @@ with open("C:\\Users\\hanna\\Desktop\\Python-challenge\\Starter_Code\\Starter_Co
 #"r" represents the read mode
     reader = csv.reader(csv_file) 
     
+    total_months = 0
+    total_revenue = 0
+    revenue = []
+    month_of_change = []
+    revenue_change = 0
+    greatest_decrease = ["", 9999999]
+    greatest_increase = ["", 0]
+    revenue_change_list = []
+    revenue_average = 0
+
+        #loop through to find total months
+        for row in reader:
+            total_months += 1
+
+
     #this is the reader object
 
     for item in reader:
     # you have to loop through the document to get each data
         print(item)
 
+    #text_path = "output.txt"
 
 #Set path
+    total_months += 1
