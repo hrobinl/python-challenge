63f73b5d1bcf13dedcdc081c03435f66d81a8fbb
hrobinl <132854695+hrobinl@users.noreply.github.com>
2023-07-01 11:26:48 -0400

    Attempt1

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,32 +1,41 @@
+
+
+#Import Modules
 import csv 
 #import to use the csv module
 
-with open("C:\\Users\\hanna\\Desktop\\Python-challenge\\Starter_Code\\Starter_Code\\PyBank\\Resources\\budgetdata.csv") as csv_file: 
+with open("C:\\Users\\hanna\\Desktop\\Python-challenge\\PyBank\\Resources\\budgetdata.csv") as csv_file: 
 #"r" represents the read mode
-    reader = csv.reader(csv_file) 
+
     
-    total_months = 0
-    total_revenue = 0
-    revenue = []
-    month_of_change = []
-    revenue_change = 0
-    greatest_decrease = ["", 9999999]
-    greatest_increase = ["", 0]
-    revenue_change_list = []
-    revenue_average = 0
-
-        #loop through to find total months
-        for row in reader:
-            total_months += 1
-
-
     #this is the reader object
-
+    reader = csv.reader(csv_file) 
     for item in reader:
     # you have to loop through the document to get each data
         print(item)
 
-    #text_path = "output.txt"
+        #Set variables
+        total_months = 0
+        total_revenue = 0
+        revenue = []
+        previous_revenue = 0
+        month_of_change = []
+        revenue_change = 0
+        greatest_decrease = ["", 9999999]
+        revenue_change_list = []
+        revenue_average = 0 
+        
+        #loop through to find total months
+        for row in reader:
+            total_months += 1
+        
+        total_revenue = total_revenue + previous_revenue
+        #previous_revenue = float(row["Profit/Losses"])
+        #revenue_change_list = revenue_change_list + [revenue_change]
 
-#Set path
-    total_months += 1
+            #if the greatest increase in rev (date & amt) over enture year
+        #if revenue_change > greatest_increase[1]:
+         #  greatest_increase[1]= revenue_change
+        #   greatest_decrease[0]= row['Date']
+        #revenue_average = sum(revenue_change_list)/len(revenue_change_list)
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,26 @@
 import csv 
 #import to use the csv module
 
-with open("C:\\Users\\hanna\\Desktop\\Python-challenge\\Starter_Code\\Starter_Code\\PyPoll\\Resources\\election_data.csv") as csv_file: 
+with open("C:\\Users\\hanna\\Desktop\\Python-challenge\\PyPoll\\Resources\\election_data.csv") as csv_file: 
 #"r" represents the read mode
-    reader = csv.reader(csv_file) 
+
     
     #this is the reader object
-
+    reader = csv.reader(csv_file) 
     for item in reader:
     # you have to loop through the document to get each data
         print(item)
 
 
 #Set path
+
+
+#The total number of votes cast
+ #total_votes = 
+#A complete list of candidates who received votes
+
+#The percentage of votes each candidate won
+
+#The total number of votes each candidate won
+
+#The winner of the election based on popular vote
